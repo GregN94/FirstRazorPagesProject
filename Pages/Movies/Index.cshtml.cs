79c1f75805caea408a2a902e3a614f121f9f5efb
GregN94 <grzesiekn1001@gmail.com>
2020-09-14 23:08:03 +0200

    Working on search

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -21,9 +22,32 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = _context.Movie.OrderBy(m => m.Genre)
+                                                          .Select(m => m.Genre);
+
+            var movies = _context.Movie.AsQueryable();
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(m => m.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(m => m.Genre == MovieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
